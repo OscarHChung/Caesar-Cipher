72beb13d6cbe1274f6252cd78a550b4337aaed03
Oscar Chung <47995398+OscarHChung@users.noreply.github.com>
2019-06-20 09:12:46 -1000

    Create caesar.c

---
+++ caesar.c
@@ -0,0 +1,38 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, string argv[])
+{
+    if(argc != 2) 
+    {
+        printf("Usage: ./caesar key");
+    }
+    
+    else 
+    {
+        int o = atoi(argv[1]);
+        if(o < 0)
+        {
+            printf("Key must be positive.");
+        }
+        string plain = get_string("plaintext: ");
+        printf("ciphertext: ");
+        for(int i = 0; i < strlen(plain); i++)
+        {
+            if islower(plain[i])
+            {
+                printf("%c", (((plain[i] + o) - 97) % 26) + 97);
+            }
+            else if isupper(plain[i])
+            {
+                printf("%c", (((plain[i] + o) - 65) % 26) + 65);
+            }
+            else
+            {
+                printf("%c", plain[i]);
+            }
+        }
+    }
+}
