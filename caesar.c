9a4f1eb2e8b97fe42a601348d5fc344f1d604303
Oscar Chung <47995398+OscarHChung@users.noreply.github.com>
2019-06-28 08:45:46 -1000

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -8,24 +8,28 @@ int main(int argc, string argv[])
     if(argc != 2) 
     {
         printf("Usage: ./caesar key");
+        return 1;
+    }
+    
+    int o = atoi(argv[1]);
+    
+    if(o < 0)
+    {
+        printf("Key must be positive.");
+        return 1;
     }
     
     else 
     {
-        int o = atoi(argv[1]);
-        if(o < 0)
-        {
-            printf("Key must be positive.");
-        }
         string plain = get_string("plaintext: ");
         printf("ciphertext: ");
         for(int i = 0; i < strlen(plain); i++)
         {
-            if islower(plain[i])
+            if(islower(plain[i])) 
             {
                 printf("%c", (((plain[i] + o) - 97) % 26) + 97);
             }
-            else if isupper(plain[i])
+            else if(isupper(plain[i]))
             {
                 printf("%c", (((plain[i] + o) - 65) % 26) + 65);
             }
@@ -34,5 +38,7 @@ int main(int argc, string argv[])
                 printf("%c", plain[i]);
             }
         }
+        printf("\n");
+        return 0;
     }
 }
